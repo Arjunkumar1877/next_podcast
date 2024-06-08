190d9ec040923bf08cc207a491cd4f68b8de686a
Arjun kumar vs <arjun.tech177@gmail.com>
2024-06-09 00:39:23 +0530

    covex cloude database intergrated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,32 @@
+"use client";
+
 import React from 'react';
 import { Button } from "@/components/ui/button"
 import { podcastData } from '@/constants';
 import PodcastCard from '@/components/PodcastCard';
 import { Podcast } from 'lucide-react';
 
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+// export default function Home() {
+//   const tasks = useQuery(api.tasks.get);
+//   return (
+
+//   );
+// }
+
+
 
 const Home = () => {
+  const tasks = useQuery(api.task.get);
   return (
     <div className='mt-9 flex flex-col gap-9'>
      <section className="flex flex-col gap-5">
 <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
-
+<div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
+      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+    </div>
 <div className="podcast_grid">
 { podcastData.map(({id, imgURL, description, title})=>(
   <PodcastCard key={id} imgUrl={imgURL} title={title} description={description} podcastId={id} />
